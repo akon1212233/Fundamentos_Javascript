09d67d3f11fb4931cd9492b5a595fd2275df405e
akon1212233 <akonbustamante@liceovvh.cl>
2025-06-02 09:28:15 -0400

    poggers

---
+++ 01_Hola_mundo/script.js
@@ -0,0 +1,4 @@
+function HM(){
+  console.log("hola mundo");
+  alert("hola mundo");
+}
---
+++ 02_Tipos_de_datos/static/js/script.js
@@ -0,0 +1,12 @@
+function Colores(){
+    let colors = ["Rojo","Verde","Azul"]
+    alert(colors);
+}
+function Identidad(){
+    const usuario = {nombre:"akon", correo:"akonbustamante@liceovvh.cl", edad:16}
+    alert(JSON.stringify(usuario));
+}
+function Estudiante(){
+    let esestudiante = true;
+    alert(esestudiante);
+}
