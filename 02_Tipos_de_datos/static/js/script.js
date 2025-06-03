e0009a333bbc3cd0a96f5bf1cf691ff231a0d1f7
akon1212233 <akonbustamante@liceovvh.cl>
2025-06-03 09:16:22 -0400

    poggers2

--- 02_Tipos_de_datos/static/js/script.js
+++ 02_Tipos_de_datos/static/js/script.js
@@ -1,12 +1,16 @@
+let colors = ["Rojo ","Verde ","Azul "]
+let usuario = {nombre:"Akon", correo:"akonbustamante@liceovvh.cl", edad:16}
+let esestudiante = true;
+
 function Colores(){
-    let colors = ["Rojo","Verde","Azul"]
     alert(colors);
 }
 function Identidad(){
-    const usuario = {nombre:"akon", correo:"akonbustamante@liceovvh.cl", edad:16}
     alert(JSON.stringify(usuario));
 }
 function Estudiante(){
-    let esestudiante = true;
     alert(esestudiante);
+}
+function Resumen(){
+    alert("Hola, Soy: "+usuario.nombre+", tengo: "+usuario.edad+", mi correo es: "+usuario.correo+" y soy estudiante: "+esestudiante+", mis colores favoritos son "+colors)
 }
