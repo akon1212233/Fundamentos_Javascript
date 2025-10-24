964d84dd55e2a8e99f59f9e80db7ef51c1ad9a30
akon1212233 <akonbustamante@liceovvh.cl>
2025-10-23 21:24:34 -0300

    asd

--- 06_Bucles_while_for/static/js/script.js
+++ 06_Bucles_while_for/static/js/script.js
@@ -1,14 +1,44 @@
+const consoler = document.querySelector("#console-visual")
 function consola(text){
-    let consoler = document.querySelector("#console-visual")
-    consoler.InnerText =  text
+    consoler.value +=  text
+    consoler.scrollTop = consoler.scrollHeight
 }
-
+function clear(){
+    consoler.value = "";
+}
 document.querySelector('.problema-1').addEventListener("click", function(){
-    let n = prompt("Dime un numero")
-    let contador = 1
+    let n = parseInt(prompt("Dime un numero"))
+    clear()
+    let contador = 0
     while(contador <= n){
-        console.log(contador)
-        consola(contador)
+        consola(contador + "\n")
         contador+=1
+    }
+})
+document.querySelector('.problema-2').addEventListener("click", function(){
+    let n = parseInt(prompt("Dime un numero"))
+    clear()
+    while(n >= 0){
+        console.log(n)
+        consola(n + "\n")
+        n-=1
+    }
+})
+document.querySelector('.problema-3').addEventListener("click", function(){
+    let n = parseInt(prompt("Dime un numero"))
+    clear()
+    let suma = 0
+    for(let con = 1 ; con <= n; con++){
+        suma += con
+    }
+    consola(suma + "\n")
+})
+document.querySelector('.problema-4').addEventListener("click", function(){
+    let n = parseInt(prompt("Dime un numero"))
+    clear()
+    for(let con = 1; con <= n ; con++){
+        if(con % 2 == 0){
+            consola(con + "\n")
+        }
     }
 })
